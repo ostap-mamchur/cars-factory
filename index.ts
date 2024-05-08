1cbfc8ecd0af564bfd2a6891710d8e419079f66f
Nazar Lysak <>
2024-05-08 20:52:33 +0300

    Added interfaces and factory

--- index.ts
+++ index.ts
@@ -3,3 +3,33 @@ interface IEngine {}
 interface IBody {}
 
 interface IAccessor {}
+
+class Engine implements IEngine {}
+
+class Body implements IBody {}
+
+class Accessor implements IAccessor {}
+
+
+
+interface ICarPartsFactory {
+    
+    createEngine(): IEngine
+    createBody(): IBody
+    createAccessor(): IAccessor
+}
+
+class CarPartsFactory implements ICarPartsFactory {
+
+    createEngine(): IEngine {
+        return new Engine()
+    }
+
+    createBody(): IBody {
+        return new Body()
+    }
+
+    createAccessor(): IAccessor {
+        return new Accessor()
+    }
+}
